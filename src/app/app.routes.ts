f2f55fb1e8008f0bf115088185830ba35467a62c
ikhzan <ikhzan.mohammad@gmail.com>
2025-06-15 17:38:56 +0300

    add structure pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { TeamComponent } from './team/team.component';
+import { KpiComponent } from './kpi/kpi.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'auth', component: AuthComponent},
+    { path: 'kpi', component: KpiComponent},
+    { path: 'tasks', component: TasksComponent},
+    { path: 'teams', component: TeamComponent},
+    { path: 'users', component: UsersComponent},
+];
